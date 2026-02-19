ae8fa498cb4542c2a7636feb24ba5bd90b61e626
visualizeai <limpidopedro+7@gmail.com>
2026-02-19 02:51:10 -0300

    init: src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App(){
+  return (
+    <div style={{padding:24, fontFamily:'system-ui'}}>
+      <h1>wwssrsdffxf</h1>
+      <p>Projeto criado automaticamente. Agora peça mudanças no chat 🙂</p>
+    </div>
+  )
+}
